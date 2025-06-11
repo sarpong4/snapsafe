75f9f49ffebe9f0e7f60407f10b12f9838571df9
Onesimus Wiafe <sarpnissi4@gmail.com>
2025-06-11 20:04:10 +0000

    add single password enforcement

--- src/actions/mod.rs
+++ src/actions/mod.rs
@@ -15,11 +15,21 @@ pub mod gc;
 pub mod registry;
 pub mod snapshot;
 
-pub fn backup_file(source: &str, target: &str) -> io::Result<()> {
+pub fn backup_data(source: &str, target: &str) -> io::Result<()> {
     let password = common::read_password();
 
     let src = Path::new(source);
     let dest = Path::new(target);
+    
+    if !src.exists() {
+        eprintln!("Source directory does not exist.");
+        let err = common::get_error(common::SnapError::Backup);
+        return Err(err);
+    }
+
+    if !dest.exists() {
+        fs::create_dir_all(&dest)?;
+    }
 
     let blobs_dir = dest.join("blobs");
     let snapshot_dir = dest.join("snapshot");
@@ -28,6 +38,18 @@ pub fn backup_file(source: &str, target: &str) -> io::Result<()> {
     fs::create_dir_all(&snapshot_dir)?;
 
     let mut registry = common::get_registry();
+    let entry = registry.find_entry(src.to_path_buf(), dest.to_path_buf());
+
+    let password_hash = common::hash_password(&password);
+
+    // strict no password change enforcement.
+    if let Some(ent) = entry {
+        if password_hash != ent.passsword_hash {
+            eprintln!("Backup destination already initialized with a different password. You cannot backup to this destination with a different password");
+            let err = common::get_error(common::SnapError::Backup);
+            return Err(err);
+        }
+    };
 
     // we need the latest json file if there is any
     let latest_json = if snapshot_dir.try_exists().unwrap() {
@@ -57,7 +79,7 @@ pub fn backup_file(source: &str, target: &str) -> io::Result<()> {
         ent = en.clone();
     }
     else {
-        ent = BackupEntry::new(snap.timestamp, src.to_path_buf(), dest.to_path_buf());
+        ent = BackupEntry::new(snap.timestamp, src.to_path_buf(), dest.to_path_buf(), password_hash);
     }
 
     let _ = registry.add_backup(ent);
@@ -116,22 +138,16 @@ pub fn restore(nth: u8, source: &str, target: &str) -> io::Result<()> {
                     }
                 },
                 Err(err) => {
-                    let er = common::get_error();
-                    eprintln!("[ERROR] Failed to decrypt file {:?} : {err}", path);
+                    let er = common::get_error(common::SnapError::Restore);
+                    eprintln!("[ERROR] Failed to decrypt data at {:?} : {err}", path);
                     return Err(er); // error indistinguishability.
                 }
             }
         }
 
-        let mut registry = common::get_registry(); 
-        let entry = common::remove_snapshot(&registry, output_dir.to_path_buf()).unwrap();
-
-        let _ = registry.add_backup(entry);
-        let _ = registry.save_to_file();
-
     }
     else {
-        let err = common::get_error();
+        let err = common::get_error(common::SnapError::Restore);
         eprintln!("Failed to restore: \n{err}");
         return Err(err);
 
@@ -167,7 +183,7 @@ pub fn delete(nth: u8, origin: &str, force: bool) -> io::Result<()> {
     let target = Path::new(origin);
 
     if !target.try_exists().unwrap_or(false) {
-        let err = common::get_error();
+        let err = common::get_error(common::SnapError::Delete);
         eprintln!("Target does not exist");
         return Err(err);
     }
@@ -177,6 +193,12 @@ pub fn delete(nth: u8, origin: &str, force: bool) -> io::Result<()> {
 
     let blob_dir = target.join("blobs");
     let snapshot_dir = target.join("snapshot");
+
+    if !blob_dir.exists() || !snapshot_dir.exists() {
+        let err = common::get_error(common::SnapError::Delete);
+        eprintln!("Target does not contain any backup");
+        return Err(err);
+    }
 
     let nth_snapshot = if snapshot_dir.try_exists().unwrap() {
         common::get_nth_recent_json_snapshot(nth, &snapshot_dir)?.map(|p| PathBuf::from(p))
@@ -198,7 +220,7 @@ pub fn delete(nth: u8, origin: &str, force: bool) -> io::Result<()> {
                 match crypto::decrypt_file_bytes(&ciphertext, &key, &nonce_bytes) {
                     Ok(_) => {
                         if let Err(_) = fs::remove_file(hash_path) {
-                            let err = common::get_error();
+                            let err = common::get_error(common::SnapError::Delete);
                             return Err(err);
                         }
 
@@ -207,15 +229,15 @@ pub fn delete(nth: u8, origin: &str, force: bool) -> io::Result<()> {
                         // BUT THE SNAPSHOT VERSION FILE IS ALSO DELETED.
                     },
                     Err(err) => {
-                        let er = common::get_error();
-                        eprintln!("Failed to decrypt file {:?}: {err}", path);
+                        let er = common::get_error(common::SnapError::Delete);
+                        eprintln!("Failed to decrypt data at {:?}: {err}", path);
                         return Err(er);
                     }
                 }
             }
         }
         if let Err(_) = fs::remove_file(snap_path) {
-            let err = common::get_error();
+            let err = common::get_error(common::SnapError::Delete);
             return Err(err);
         }
         let mut registry = common::get_registry(); 
@@ -225,7 +247,7 @@ pub fn delete(nth: u8, origin: &str, force: bool) -> io::Result<()> {
         let _ = registry.save_to_file();
     }
     else {
-        let err = common::get_error();
+        let err = common::get_error(common::SnapError::Delete);
         eprintln!("Failed to delete backup: Invalid password or unreadable metadata: {err}");
         return Err(err);
     }
@@ -236,20 +258,24 @@ pub fn delete(nth: u8, origin: &str, force: bool) -> io::Result<()> {
 }
 
 pub fn list_from_registry() -> io::Result<()> {
-    println!("Listing All Backups 📦...");
     let registry = common::get_registry().registry;
 
-    for entry in registry {
-        println!(
-            "- ID: {}\n Original Path: {:?}\n Backup Path: {:?}\n Created: {}\n Snapshots: {}",
-            entry.id,
-            entry.origin_path,
-            entry.backup_path,
-            entry.timestamp,
-            entry.snapshot_count,
-        )
+    if registry.is_empty() {
+        println!("No data has been backed up yet!");
+    }
+    else {
+        println!("Listing All Backups 📦...");
+        for entry in registry {
+            println!(
+                "- ID: {}\n Original Path: {:?}\n Backup Path: {:?}\n Created: {}\n Snapshots: {}",
+                entry.id,
+                entry.origin_path,
+                entry.backup_path,
+                entry.timestamp,
+                entry.snapshot_count,
+            )
+        }
     }
     
     Ok(())
 }
-
