e0263a8d59084cb93d60d7946ebd3508e92baff3
Onesimus Wiafe <sarpnissi4@gmail.com>
2025-06-23 20:48:26 +0000

    add logic to consider decompression

--- src/actions/restore.rs
+++ src/actions/restore.rs
@@ -1,9 +1,38 @@
 use std::{fs, io, path::{Path, PathBuf}};
 
-use crate::{actions::crypto, utils::{self, snapshot::Snapshot}};
+use crate::{actions::crypto, compress, utils::{self, snapshot::Snapshot, SnapError}};
 
+/// Restore the nth version of a backup at the location: `src`
+/// Returns: `Ok(())` when successful or `Err` on any kind of failure.
+/// 
+/// if `nth` is one, we restore the latest backup. 
+/// With default `GarbageCollectorConfig`, we keep 3 versions of each backup and hence 
+/// nth is in the range 0..3
+/// 
+/// When a user defines a garbage collector config limit Lim, we define nth to be in the range `0..Lim`.
+/// We make decompresssion based on the compression algorithm used during backup.
+/// 
+/// Decryption first occurs then decompression will take place.
+/// the decompressed content is written to a file and saved in a path format similar to when backup occured. 
+/// The `output_dir` is where the final files will be written to.
 pub fn restore(nth: usize, src: &Path, output_dir: &Path) -> io::Result<()> {
     let password = utils::read_password();
+
+    let mut registry = utils::get_registry();
+    let entry = registry.find_entry_from_dest(src.to_path_buf());
+
+    let comp = {
+        if let Some(ent) = entry {
+            ent.clone().compression_algorithm
+        }
+        else {
+            let err = utils::get_error(SnapError::Restore);
+            return Err(err);
+        }
+    };
+
+    let engine = compress::build_decompression_engine(comp)?;
+
 
     if !output_dir.try_exists().unwrap_or(false) {
         let _ = fs::create_dir_all(&output_dir)?;
@@ -38,8 +67,10 @@ pub fn restore(nth: usize, src: &Path, output_dir: &Path) -> io::Result<()> {
                     if let Some(parent) = rel_target.parent() {
                         let _ = fs::create_dir_all(parent)?;
                     }
+
+                    let decompressed_content = engine.decompress(&decrytped)?;
                     
-                    if let Err(err) = fs::write(&rel_target, &decrytped) {
+                    if let Err(err) = fs::write(&rel_target, &decompressed_content) {
                         eprintln!("Could not write to restore file: {err}");
                         return Err(err);
                     }
@@ -51,7 +82,6 @@ pub fn restore(nth: usize, src: &Path, output_dir: &Path) -> io::Result<()> {
                 }
             }
         }
-        let mut registry = utils::get_registry();
         if let Some(entry) = utils::remove_snapshot(&mut registry, src.to_path_buf()) {
             let _ = registry.add_backup(entry);
             let _ = registry.save_to_file();
